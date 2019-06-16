aaeca5fd81841a67c1ae35944d5af8d50a2c4893
RizkiFadhoni <48019329+RizkiFadhoni@users.noreply.github.com>
2019-06-17 05:05:43 +0700

    tabview

---
+++ app/src/main/java/com/rahgena/dlatih/view/Adapter/CustomAdapter.java
@@ -0,0 +1,4 @@
+package com.rahgena.dlatih.view.Adapter;
+
+public class CustomAdapter {
+}
--- app/src/main/java/com/rahgena/dlatih/view/fragment/home.java
+++ app/src/main/java/com/rahgena/dlatih/view/fragment/home.java
@@ -5,14 +5,20 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.rahgena.dlatih.R;
+import com.rahgena.dlatih.view.Adapter.GridAdapter;
 
 public class home extends Fragment {
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
     public home() {
         // Required empty public constructor
     }
@@ -22,6 +28,14 @@ public class home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new GridLayoutManager(getActivity(),2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new GridAdapter();
+        mRecyclerView.setAdapter(mAdapter);
         return view;
     }
 }
